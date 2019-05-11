40c5a0f0abb45d7cfa532f817b6a7563bb8b808e
Guillermo Garcia Jr <guillermo@Guillermos-MacBook-Pro.local>
2019-05-10 19:10:35 -0700

    Updated branch with login functionality attempt, but Axios requests are resolving a 400 error.

---
+++ routes/api/apiRoutes.js
@@ -0,0 +1,60 @@
+require('dotenv').config();
+const unirest = require("unirest");
+const axios = require("axios");
+const router = require("express").Router();
+
+// router.get("/recipes", (req, res) => {
+//   console.log("hell");
+//   axios
+//     .get("http://www.recipepuppy.com/api/", { params: req.query })
+//     .then(({ data: { results } }) => res.json(results))
+//     .catch(err => res.status(422).json(err));
+// });
+
+router.post("/recipes/search", (req, res) => {
+  console.log(req.query.recipe);
+  unirest
+    .get(
+      `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?number=10&offset=0`
+    )
+    .query({
+      query: req.query.recipe
+    })
+    .header(
+      "X-RapidAPI-Host",
+      process.env.REACT_APP_GENERAL_RECIPE_HOST
+    )
+    .header(
+      "X-RapidAPI-Key",
+      process.env.REACT_APP_GENERAL_RECIPE_KEY
+    )
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      res.status(422).json(err);
+    });
+});
+
+router.post("/recipes/id", (req, res) => {
+  unirest
+    .get(
+      `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${req.query.id}/information`
+    )
+    .header(
+      "X-RapidAPI-Host",
+      process.env.REACT_APP_DETAIL_RECIPE_HOST
+    )
+    .header(
+      "X-RapidAPI-Key",
+      process.env.REACT_APP_DETAIL_RECIPE_KEY
+    )
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      res.status(422).json(err);
+    });
+});
+
+module.exports = router;

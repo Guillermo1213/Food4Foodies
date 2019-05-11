9f9865443522c78ca0310ce49aed0c23d805462d
Guillermo Garcia Jr <guillermo@Guillermos-MacBook-Pro.local>
2019-05-11 11:55:19 -0700

    Login funcionality is now working.

--- client/src/pages/RecipeByID.js
+++ client/src/pages/RecipeByID.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import { Row, Col } from "../components/Grid";
 import API from "../utils/API";
+import Thumbnail from "../components/Thumbnail";
+import { Link } from "react-router-dom";
 
 export default class RecipeByID extends Component {
   state = {
-    recipeDetails: {}
+    recipeDetails: {
+      extendedIngredients: []
+    }
   };
 
   componentDidMount() {
@@ -17,16 +21,27 @@ export default class RecipeByID extends Component {
   }
 
   render() {
+    console.log(this.state.recipeDetails);
     return (
-      <div>
-        <Row>
-          <Col size="xs-12">{this.state.recipeDetails.title}</Col>
-        </Row>
-        <Row>
-          <Col size="xs-6">{this.state.recipeDetails.image}</Col>
-          <Col size="xs-6">{this.state.recipeDetails.instructions}</Col>
-        </Row>
-      </div>
+      <li className="list-group-item">
+        <div className="divStyle">
+          <Row className="text-center">
+            <Col size="md-12">{this.state.recipeDetails.title}</Col>
+            <Link to={"/"}>Recipes</Link>
+          </Row>
+          <Row>
+            <Col size="xs-12 sm-4">
+              <Thumbnail src={this.state.recipeDetails.image} />
+            </Col>
+            <Col size="xs-12 sm-4">{this.state.recipeDetails.instructions}</Col>
+            <Col size="xs-12 sm-4">
+              {this.state.recipeDetails.extendedIngredients.map(recipe => (
+                <li>{recipe.name}</li>
+              ))}
+            </Col>
+          </Row>
+        </div>
+      </li>
     );
   }
 }
--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 const express = require("express");
 const path = require("path");
-
-const PORT = process.env.PORT || 3001;
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const session = require('express-session')
+const dbConnection = require('./database') 
+const MongoStore = require('connect-mongo')(session)
+const passport = require('./passport');
+const PORT = process.env.PORT || 8080;
 const app = express();
+const user = require('./routes/user')
 const apiRoutes = require("./routes/apiRoutes");
+
+
+// MIDDLEWARE
+app.use(morgan('dev'))
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+)
+app.use(bodyParser.json())
+
+// Sessions
+app.use(
+	session({
+		secret: 'fraggle-rock', //pick a random string to make the hash that is generated secure
+		store: new MongoStore({ mongooseConnection: dbConnection }),
+		resave: false, //required
+		saveUninitialized: false //required
+	})
+)
+
+// Passport
+app.use(passport.initialize())
+app.use(passport.session()) // calls the deserializeUser
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -15,12 +45,13 @@ if (process.env.NODE_ENV === "production") {
 
 // Use apiRoutes
 app.use("/api", apiRoutes);
+app.use('/user', user)
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// // Send every request to the React app
+// // Define any API routes before this runs
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);

0bd936406d78fd293a79aac3aa81777ce51e712e
Sonia D <davson1987@gmail.com>
2019-05-16 02:19:00 -0700

    added some styling to recipeByID to use Tabs

--- client/src/pages/RecipeByID.js
+++ client/src/pages/RecipeByID.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
-import { Row, Col } from "../components/Grid";
+import { Row, Col, Container } from "../components/Grid";
+import Tabs from "../components/Tabs";
 import API from "../utils/API";
 import Thumbnail from "../components/Thumbnail";
 import Select from "../components/Select";
-import { Link } from "react-router-dom";
 import axios from "axios";
+import { Grid, Paper } from "@material-ui/core";
+
+const style = {
+  Paper: { padding: 20, marginTop: 10, marginBotton: 10 }
+};
 
 export default class RecipeByID extends Component {
   state = {
@@ -35,7 +40,9 @@ export default class RecipeByID extends Component {
     //request to server to add groceries
     axios
       .put("/groceries/addMeal", {
-        groceries: this.state.recipeDetails.extendedIngredients,
+        groceries: this.state.recipeDetails.extendedIngredients.map(
+          recipe => recipe.name
+        ),
         day: this.state.day,
         meal: this.state.mealSlot
       })
@@ -51,6 +58,19 @@ export default class RecipeByID extends Component {
       });
   };
 
+  // handleSubmit(event) {
+  // 	console.log('Add meal')
+  // 	event.preventDefault()
+
+  //   const JSX_MODAL = (
+  //     <div className="ui dimmer modals visible active">
+  //       <div className="ui standard modal visible active">
+  //         THIS IS SOME TEXT IN THE MODAL // add some UI features here
+  //       </div>
+  //     </div>
+  //   );
+  // }
+
   handleSubmitFavorite = event => {
     console.log("Add meal");
     event.preventDefault();
@@ -59,7 +79,7 @@ export default class RecipeByID extends Component {
     axios
       .put("/favorites/add", {
         imgURl: this.state.recipeDetails.image,
-        recipeTitle: this.state.recipeDetails.title,
+        recipeTitle: this.state.recipeDetails.title
       })
       .then(response => {
         console.log(response);
@@ -73,53 +93,81 @@ export default class RecipeByID extends Component {
       });
   };
 
-
-
   render() {
     console.log(this.state.recipeDetails);
     return (
-      <li className="list-group-item">
-        <div className="divStyle">
-          <Row className="text-center">
-            <Col size="md-12">{this.state.recipeDetails.title}</Col>
-            <Link to={"/"}>Recipes</Link>
-          </Row>
-          <Row>
+      <div>
+        <Row className="text-center">
+          <Col size="md-12">{this.state.recipeDetails.title}</Col>
+        </Row>
+        <Row>
+          <Col size="xs-12 sm-4">
+            <Thumbnail src={this.state.recipeDetails.image} />
+          </Col>
+          <Col size="xs-12 sm-4">{this.state.recipeDetails.instructions}</Col>
+          {!this.state.recipeDetails.extendedIngredients ? (
+            <div />
+          ) : (
             <Col size="xs-12 sm-4">
-              <Thumbnail src={this.state.recipeDetails.image} />
+              {this.state.recipeDetails.extendedIngredients.map(recipe => (
+                <li>{recipe.name}</li>
+              ))}
             </Col>
-            <Col size="xs-12 sm-4">{this.state.recipeDetails.instructions}</Col>
-            {!this.state.recipeDetails.extendedIngredients ? (
-              <div />
-            ) : (
-              <Col size="xs-12 sm-4">
-                {this.state.recipeDetails.extendedIngredients.map(recipe => (
-                  <li>{recipe.name}</li>
-                ))}
-              </Col>
-            )}
-          </Row>
+          )}
+        </Row>
+        <div>
+          <Select
+            onSelectChange={this.handleSelectChange}
+            mealSlot={this.state.mealSlot}
+            day={this.state.day}
+          />
+          <button
+            className="btn btn-primary col-1 col-mr-auto"
+            onClick={this.handleSubmitMeal}
+            type="submit"
+          >
+            Add Meal{" "}
+          </button>
+          <button
+            className="btn btn-primary col-1 col-mr-auto"
+            onClick={this.handleSubmitFavorite}
+            type="submit"
+          >
+            Like{" "}
+          </button>
         </div>
-        <Select
-          onSelectChange={this.handleSelectChange}
-          mealSlot={this.state.mealSlot}
-          day={this.state.day}
-        />
-        <button
-          className="btn btn-primary col-1 col-mr-auto"
-          onClick={this.handleSubmitMeal}
-          type="submit"
-        >
-          Add Meal{" "}
-        </button>
-        <button
-          className="btn btn-primary col-1 col-mr-auto"
-          onClick={this.handleSubmitFavorite}
-          type="submit"
-        >
-          Add Meal{" "}
-        </button>
-      </li>
+        <div>
+          <Grid container>
+            <Grid item sm>
+              <Paper style={style.Paper}>
+                <Thumbnail src={this.state.recipeDetails.image} />
+              </Paper>
+            </Grid>
+            <Grid item sm>
+              <Paper style={style.Paper}>
+                <div>
+                  <h1>{this.state.recipeDetails.title}</h1>
+                  <Tabs>
+                    <div label="Instructions">
+                      {this.state.recipeDetails.instructions}
+                    </div>
+                    <div label="Ingredients">
+                      {this.state.recipeDetails.extendedIngredients.map(
+                        recipe => (
+                          <li>{recipe.name}</li>
+                        )
+                      )}
+                    </div>
+                    <div label="Sarcosuchus">
+                      Nothing to see here, this tab is <em>extinct</em>!
+                    </div>
+                  </Tabs>
+                </div>
+              </Paper>
+            </Grid>
+          </Grid>
+        </div>
+      </div>
     );
   }
 }

b217dc87835578923418adad58a08d6aa138f671
Sonia D <davson1987@gmail.com>
2019-05-15 19:38:40 -0700

    added an albums component and cleaned up Navbar

--- routes/groceries.js
+++ routes/groceries.js
@@ -2,15 +2,18 @@ const express = require("express");
 const router = express.Router();
 const User = require("../database/models/user");
 
-router.put("/addMeal", function (req, res) {
+router.put("/addMeal", function(req, res) {
   console.log("routes/groceries.js, adding ");
 
   const { groceries, meal, day } = req.body;
   console.log(req.user._id);
-  User.findByIdAndUpdate({ _id: req.user._id }, { groceries: groceries }, (addedGroceries) => {
-    res.json(addedGroceries);
-  });
+  User.findByIdAndUpdate(
+    { _id: req.user._id },
+    { groceries: groceries },
+    addedGroceries => {
+      res.json(addedGroceries);
+    }
+  );
 });
-
 
 module.exports = router;
